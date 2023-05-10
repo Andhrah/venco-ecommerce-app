4c8a74591c27e5b9354b1f01fd3256fd709e98df
Alexandra Collins <andracolexa@gmail.com>
2023-05-10 16:12:48 +0100

    scaffold login component

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
+
+const Login = ():JSX.Element => {
+  const { container, text } = styles;
+  return (
+    <View style={container}>
+      <Text style={text}>Coming Soon...</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: hp('100%'),
+    width: wp('100%'),
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: hp(4.4),
+    fontWeight: '700',
+    color: '#172B4E',
+  },
+});
+
+export default Login;
